c329890c8197468ae3ea326340a8049b12a34d0a
Josh Batchelor <josh_batch@live.com.au>
2014-12-27 22:43:43 +0800

    So much trouble getting shift algorithm. Seems to be working now
    Should probably write tests and shiz

--- CaesarCipherCracker.java
+++ CaesarCipherCracker.java
@@ -3,8 +3,11 @@ public class CaesarCipherCracker
 	public static void main(String args[])
 	{
 		LetterFrequency lf = new LetterFrequency("This is my message");
-//		lf.inc('c');
+		LetterFrequency lf2 = LetterFrequency.shift(lf, 27);
+
 		int[] a = lf.getFreqArray();
 		System.out.println(lf.toString());
+		System.out.println(lf2.toString());
+		//System.out.println("" + lf.getAverageProportionDifference(lf2));
 	}
 }
--- LetterFrequency.java
+++ LetterFrequency.java
@@ -2,12 +2,18 @@ public class LetterFrequency
 {
 
 	private int[] frequencies;
+	private double[] proportions;
+	private int count;
 
 	public LetterFrequency(String string)
 	{
 		char[] stringArray = string.toCharArray();
 
+		count = stringArray.length;
+
 		frequencies = new int[26];
+		proportions = new double[26];
+
 		for(int i = 0; i < 26; i++)
 		{
 			frequencies[i] = 0;
@@ -17,11 +23,38 @@ public class LetterFrequency
 		{
 			inc(c);
 		}
+
+		for(int i = 0; i < 26; i++)
+		{
+			proportions[i] = frequencies[i]/(double)count;
+		}
+	}
+
+	public LetterFrequency(int[] inFrequencies, int inCount)
+	{
+		proportions = new double[26];
+		frequencies = inFrequencies;
+		count = inCount;
+
+		for(int i = 0; i < 26; i++)
+		{
+			proportions[i] = frequencies[i]/(double)count;
+		}
 	}
 
 	public int[] getFreqArray()
 	{
 		return frequencies;
+	}
+
+	public double[] getProportions()
+	{
+		return proportions;
+	}
+
+	public int getCount()
+	{
+		return count;
 	}
 
 	public void inc(char c)
@@ -48,10 +81,46 @@ public class LetterFrequency
 		s += "{";
 		for(int i = 0; i < 25; i++)
 		{
-			s += (char)('a' + i) + "=" + frequencies[i] + ", ";
+			s += (char)('a' + i) + "=" + frequencies[i] + ":" + ((double)((int)(10000 * proportions[i]))/100) + "%"+ ", ";
 		}
 		s += "z=" + frequencies[25] + "}";
 
 		return s;
 	}
+
+	public static LetterFrequency shift(LetterFrequency letterFrequency, int shift)
+	{
+		int[] newFrequencies = new int[26];
+
+		for(int i = 0; i < 26; i++)
+		{
+			newFrequencies[i] = letterFrequency.getFreqArray()[(i + (Math.abs(26 - shift%26)))%26];
+		}
+
+		LetterFrequency newLetterFrequency = new LetterFrequency(newFrequencies, letterFrequency.getCount());
+		return newLetterFrequency;
+	}
+
+	public double getAverageProportionDifference(LetterFrequency inLetterFrequency)
+	{
+		double totalDiff = 0, averageDiff;
+
+		for(int i = 0; i < 26; i++)
+		{
+			totalDiff += Math.abs(frequencies[i] - inLetterFrequency.getFreqArray()[i]);
+		}
+
+		averageDiff = totalDiff / 26;
+
+		return averageDiff;
+	}
+
+
+
+
+
+
+
+
+
 }
